8f26f830398c771387bb9132f3558da82f68d3ef
pierosanchez098 <128500925+pierosanchez098@users.noreply.github.com>
2023-09-20 19:21:06 +0200

    Add files via upload

---
+++ NumerosAleatorios.java
@@ -0,0 +1,18 @@
+package a;
+
+import java.util.Random;
+
+public class NumerosAleatorios {
+    public static void main(String[] args) {
+        // Crear un objeto Random
+        Random random = new Random();
+
+        // Generar y mostrar 5 números aleatorios entre 0 y 100
+        System.out.println("Números aleatorios entre 0 y 100:");
+        for (int i = 0; i < 5; i++) {
+            int numeroAleatorio = random.nextInt(101); // Genera números entre 0 (inclusive) y 101 (exclusivo)
+            System.out.println(numeroAleatorio);
+        }
+    }
+}
+
